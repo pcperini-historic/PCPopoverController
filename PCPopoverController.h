c191bcae883bf926754e9a0953ac52e113d8e750
Patrick Perini <pcperini@aim.com>
2012-05-25 19:53:29 -0300

    Update PCPopoverController.h

--- PCPopoverController.h
+++ PCPopoverController.h
@@ -3,7 +3,7 @@
 //  PCPopoverControllerTests
 //
 //  Created by Patrick Perini on 5/16/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Licensing information available in README.md
 //
 
 #import <UIKit/UIKit.h>
